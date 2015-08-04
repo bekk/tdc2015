57661a5e395236436ec0c2d0131fc87e2740df1b
Magnus Fjell <magnus.fjell@gmail.com>
2015-08-04 10:22:43 +0200

    Move ball side to side

--- src/index.js
+++ src/index.js
@@ -3,6 +3,31 @@ var paddleHeight = 120;
 
 document.querySelector('.paddle-left').style.top = 200;
 document.querySelector('.paddle-right').style.top = 200;
+var ball = document.querySelector('.ball');
+ball.style.top = 250;
+ball.style.left = 250;
+
+
+var goingRight = true;
+window.setInterval(function() {
+  var ball = document.querySelector('.ball');
+  var left = parseInt(ball.style.left, 10);
+  // console.log("1");
+  if(goingRight){
+    ball.style.left = left + 5;
+  }
+  else {
+    ball.style.left = left - 5;
+  }
+  // console.log(left);
+  if(left > parseInt(window.innerWidth, 10)-20-20) {
+    goingRight = false;
+  }
+  else if (left < 0 + 20) {
+    goingRight = true;
+  }
+}, 10);
+
 
 document.addEventListener("keydown", function(e){
   if(e.which == 87) { //w
