4df19d41ab2052e6d146afa6f43a293455f89f4a
Magnus Fjell <magnus.fjell@gmail.com>
2015-08-31 08:42:39 +0200

    Add file for controlling paddles with myo

---
+++ src/pong/paddlesMyo.js
@@ -0,0 +1,35 @@
+var Myo = require("myo");
+var data = {};
+var myo1;
+var myo2;
+
+module.exports = function(initialData) {
+  data = initialData;
+  myo1 = Myo.myos[0];
+  myo2 = Myo.myos[1];
+  myo1.off("orientation")
+  myo2.off("orientation")
+  myo1.on("orientation", orientationFunction.bind(undefined, data.left));
+  myo2.on("orientation", orientationFunction.bind(undefined, data.right));
+};
+
+var orientationFunction = function(topObj, orientationData){
+  if(data.finished) { return; }
+
+  var half = data.maxHeight/2;
+  var per30 = half/30;
+
+  if(orientationData.y < 0) { //ned
+    var orientation = Math.max(orientationData.y, -0.30) * -100; //0...30
+
+    var offset = per30 * orientation;
+    var newHeight = offset + half ;
+    topObj.top = Math.min(newHeight, data.maxHeight - data.paddleHeight);
+
+  } else { // opp
+    orientation = Math.min(orientationData.y, 0.30) * 100; // 0...30
+    offset = per30 * orientation;
+    newHeight = half - offset;
+    topObj.top = Math.max(newHeight, 0);
+  }
+};
