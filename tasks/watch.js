2e3e0e79ccffd1b6f48e1297fefdc7dea1eaf26c
Mikael Brevik <mikaelbre@gmail.com>
2015-10-05 21:46:40 +0200

    Initial minimalist design

--- tasks/watch.js
+++ tasks/watch.js
@@ -3,10 +3,12 @@ var build = require("./build");
 var less = require("./less");
 var path = require("path");
 
-var js = path.join(__dirname, 'apps', '**', '*.js');
-var css = path.join(__dirname, 'apps', '**', '*.less');
+var js = path.join(__dirname, '..', 'apps', '**', '*.js');
+var css = path.join(__dirname, '..', 'apps', '**', '*.less');
+var base = path.join(__dirname, '..', 'style', '**', '*.less');
 
 module.exports = function watch(apps) {
   gulp.watch(js, build.bind(null, apps));
   gulp.watch(css, less.bind(null, apps));
+  gulp.watch(base, less.bind(null, apps));
 };
